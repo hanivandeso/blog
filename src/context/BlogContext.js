0919e53913b2344c938585168c9786095d2d748f
hanivandeso <hanivandeso@gmail.com>
2020-01-14 11:56:02 +0700

    Delete Button

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,19 +2,34 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "delete_blogpost":
+      return state.filter(blogPost => blogPost.id !== action.payload);
     case "add_blogpost":
-      return [...state, { title: `Blog Post #${state.length + 1}` }]; //make array and put into it
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 999999),
+          title: `Blog Post #${state.length + 1}`
+        }
+      ]; //make array and put into it
     default:
       return state;
   }
 };
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
 
-const addBlogPost = () => {
-  dispatch({ type: "add_blogpost" });
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
+  };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
